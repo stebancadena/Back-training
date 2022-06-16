244f4e9ec48b53357ff455f5348679fe246960dc
Steban Cadena <Steban.Cadena@jalasoft.com>
2022-06-16 13:17:36 -0500

    Modify the unit test to adapt them to the changes of the pagination

--- APM-Back.Tests/ProductsControllerTest.cs
+++ APM-Back.Tests/ProductsControllerTest.cs
@@ -2,12 +2,15 @@
 using APM_Back.Data;
 using APM_Back.Models;
 using APM_Back.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -17,18 +20,29 @@ namespace APM_Back.Tests.Controllers
     public class ProductsControllerTest
     {
         private readonly Mock<IProductService> _mockRepo;
+        private readonly Mock<IUriService> _mockUri;
+        private readonly Mock<PaginationFilter> _paginationFilter;
         private readonly ProductsController _controller;
         public ProductsControllerTest()
         {
+            var httpContext = new DefaultHttpContext();
+
             _mockRepo = new Mock<IProductService>();
-            _controller = new ProductsController(_mockRepo.Object);
+            _mockUri = new Mock<IUriService>();
+            _paginationFilter = new Mock<PaginationFilter>();
+            _controller = new ProductsController(_mockRepo.Object, _mockUri.Object);
+
+            _controller.ControllerContext = new ControllerContext() { HttpContext = new DefaultHttpContext() };
         }
 
         //This was just for testing
         [Fact]
         public async void GetAll_Executions_Type()
         {
-            var response = await _controller.GetProducts();
+            _mockRepo.Setup(repo => repo.GetAll(_paginationFilter.Object))
+                .ReturnsAsync(new PagedData(new List<Product>() { new Product(), new Product() }, 2));
+
+            var response = await _controller.GetProducts(_paginationFilter.Object);
             Assert.IsType<OkObjectResult>(response);
             //Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
@@ -36,14 +50,14 @@ namespace APM_Back.Tests.Controllers
         [Fact]
         public async void GetAll_Executes_ReturnsExactNumberOfProducts()
         {
-            _mockRepo.Setup(repo => repo.GetAll())
-                .ReturnsAsync(new List<Product>() { new Product(), new Product() });
+            _mockRepo.Setup(repo => repo.GetAll(_paginationFilter.Object))
+                .ReturnsAsync(new PagedData(new List<Product>() { new Product(), new Product() } , 2));
 
-            var response = await _controller.GetProducts();
+            var response = await _controller.GetProducts(_paginationFilter.Object);
 
             var okResult = response as OkObjectResult;
-            var config = okResult.Value as List<Product>;
-            Assert.Equal(2, config.Count);
+            var config = okResult.Value as PagedResponse<IEnumerable<Product>>;
+            Assert.Equal(2, config.Data.Count());
         }
     }
 }
